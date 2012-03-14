705709a38201db16a0ba13e18c0b3a8b3e803df2
Jarrell Waggoner <malloc47@gmail.com>
2012-03-13 22:37:07 -0400

    this should have been rewritten ages ago... strike that, it should never have been written that way to begin with...

--- matlab/conditionlabels.m
+++ matlab/conditionlabels.m
@@ -1,29 +1,15 @@
 function new_labels = conditionlabels(labels)
 
-counter=0;
-
 new_labels = labels;
+size(labels)
+[dm,dm_idx] = bwdist(labels>0);
 
-while(min(min(new_labels) < 1) || counter > 50)
-counter
-for i = 1:size(labels,1)
-    for j = 1:size(labels,2)
-        if labels(i,j) == 0
-            if i~=size(labels,1) && labels(i+1,j) ~= 0
-                new_labels(i,j) = labels(i+1,j);
-            elseif j~=size(labels,2) && labels(i,j+1) ~= 0
-                new_labels(i,j) = labels(i,j+1);
-            elseif i~=1 && labels(i-1,j) ~= 0
-                new_labels(i,j) = labels(i-1,j);
-            elseif j~=1 && labels(i,j-1) ~= 0
-                new_labels(i,j) = labels(i,j-1);
-            else
-%                 disp('Complaining');
-            end
+for i=1:size(labels,1)
+    for j=1:size(labels,2)
+        if(labels(i,j)==0)
+            new_labels(i,j)=labels(dm_idx(i,j));
         end
     end
 end
-
-counter = counter+1;
 
 end
