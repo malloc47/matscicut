c5ea16b26ee86745041ad4d05f1b0305339fbcc3
Jarrell Waggoner <malloc47@gmail.com>
2012-03-25 11:16:39 -0400

    factored out some job code

--- scripts/create-watershed-jobs.sh
+++ scripts/create-watershed-jobs.sh
@@ -1,29 +1,30 @@
 #!/bin/bash
 
 j=0
-seqpath=/home/malloc47/src/projects/matsci/matscicut/seq1/watershed
-labelpath=/home/malloc47/src/projects/matsci/matscicut/seq1/watershed/labels
-groundpath=/home/malloc47/src/projects/matsci/matscicut/seq1/ground
-scriptpath=/home/malloc47/src/projects/matsci/matscicut/scripts
+seq=seq9
+seqpath=/home/malloc47/src/projects/matsci/matscicut/$seq/watershed
+labelpath=/home/malloc47/src/projects/matsci/matscicut/$seq/watershed/labels
+groundpath=/home/malloc47/src/projects/matsci/matscicut/$seq/ground
+scriptpath=/home/malloc47/src/projects/matsci/matscicut/scripts/$seq
 
-echo "#!/bin/bash" > ${j}w.sh
-for (( s=90; s<=100; s++ )) ; do
+echo "#!/bin/bash" > $seq/${j}w.sh
+for (( s=$1; s<=$2; s++ )) ; do
 
 	echo "mkdir $labelpath
-cp ${groundpath}/image$(printf '%04d' $s).label $labelpath/" >> ${j}w.sh
+cp ${groundpath}/image$(printf '%04d' $s).label $labelpath/" >> $seq/${j}w.sh
 
-	echo "#!/bin/bash" > ${s}w-b.sh
-	echo "/home/malloc47/src/projects/matsci/matscicut/scripts/${s}w-b.sh" >> ${j}w.sh
-	for (( i=$(($s-1)); i>=90; i-- )) ; do
-		echo "/usr/local/matlab/bin/matlab -nodisplay -nojvm -r \"addpath('matlab'); watershed_eval3($(($i+1)),$i); exit;\" ">> ${s}w-b.sh
+	echo "#!/bin/bash" > $seq/${s}w-b.sh
+	echo "$scriptpath/${s}w-b.sh" >> $seq/${j}w.sh
+	for (( i=$(($s-1)); i>=$1; i-- )) ; do
+		echo "/usr/local/matlab/bin/matlab -nodisplay -nojvm -r \"addpath('matlab'); watershed_eval3($(($i+1)),$i); exit;\" ">> $seq/${s}w-b.sh
 	done
-	echo "#!/bin/bash" > ${s}w-f.sh
-	echo "/home/malloc47/src/projects/matsci/matscicut/scripts/${s}w-f.sh" >> ${j}w.sh
-	for (( i=$(($s+1)); i<=100; i++ )) ; do
-		echo "/usr/local/matlab/bin/matlab -nodisplay -nojvm -r \"addpath('matlab'); watershed_eval3($(($i-1)),$i); exit;\" " >> ${s}w-f.sh
+	echo "#!/bin/bash" > $seq/${s}w-f.sh
+	echo "$scriptpath/${s}w-f.sh" >> $seq/${j}w.sh
+	for (( i=$(($s+1)); i<=$2; i++ )) ; do
+		echo "/usr/local/matlab/bin/matlab -nodisplay -nojvm -r \"addpath('matlab'); watershed_eval3($(($i-1)),$i); exit;\" " >> $seq/${s}w-f.sh
 	done
 
-	echo "mv $labelpath $seqpath/$s " >> ${j}w.sh
+	echo "mv $labelpath $seqpath/$s " >> $seq/${j}w.sh
 
 	#if [ "$((($s*2)%4))" == "0" ] ; then
 		#echo "#!/bin/bash" > $j.sh
