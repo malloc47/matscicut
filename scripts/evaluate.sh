06daa712e23426f956958108836d8fdd5ed0a95b
Jarrell Waggoner <malloc47@gmail.com>
2012-10-28 19:53:55 -0400

    make script path absolute

--- scripts/evaluate.sh
+++ scripts/evaluate.sh
@@ -3,7 +3,7 @@ echo -n "" > jobs
 for f in $(find $1 -maxdepth 3 -name \*.label | grep -v ^./ground) ; do
     # if there is groundtruth with the same name
     if [ -f ./ground/$(basename $f) -a ! -f ${f%.*}.score ] ; then
-	echo "../scripts/fmeasure.sh ./ground/$(basename $f) $f ${f%.*}.score" >> jobs
+	echo "/home/malloc47/src/projects/matsci/matsciskel/scripts/fmeasure.sh ./ground/$(basename $f) $f ${f%.*}.score" >> jobs
 	#../scripts/fmeasure.sh ./ground/$(basename $f) $f ${f%.*}.score
     fi
 done
